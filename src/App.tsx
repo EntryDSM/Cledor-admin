59b04f6b97b5758cf2f64940df580f254492c5c0
Joonmo-dev <yunedoopea610@gmail.com>
2019-05-02 16:01:59 +0900

    Add global style

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import * as React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { Account, Messages } from './pages';
+import { GlobalStyle } from './globalStyle';
 
 const App = () => {
   return (
     <Router>
+      <GlobalStyle />
       <Route path="/account" exact component={Account} />
       <Route path="/" exact component={Messages} />
     </Router>
