dc8c79049f819c79060bade8206e1bfb35d359bf
Joonmo-dev <yunedoopea610@gmail.com>
2019-05-07 22:38:22 +0900

    Add button component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,22 @@
 import * as React from 'react';
 import { Component } from './style';
 
-const Button: React.FunctionComponent = () => {
-  return <Component />;
+export interface ButtonProps {
+  onClick?: (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
+  className?: string;
+  children?: React.ReactNode;
+}
+
+const Button: React.FunctionComponent<ButtonProps> = ({
+  onClick,
+  className,
+  children = '',
+}) => {
+  return (
+    <Component onClick={onClick} className={className}>
+      {children}
+    </Component>
+  );
 };
 
 export default Button;
--- src/components/Button/style.ts
+++ src/components/Button/style.ts
@@ -1,3 +1,26 @@
 import styled from 'styled-components';
+import { Theme } from '../../globalStyle';
 
-export const Component = styled.div``;
+export const Component = styled.button`
+  display: block;
+  width: 100%;
+  height: 65px;
+  font-size: 25px;
+  color: ${Theme.MAIN_COLOR5};
+  background-color: #fff;
+  border-radius: 10px;
+  box-shadow: ${Theme.DEFAULT_SHADOW};
+  cursor: pointer;
+  transition: 0.3s box-shadow;
+
+  &:hover {
+    box-shadow: ${Theme.HOVER_SHADOW};
+  }
+  &:active {
+    box-shadow: none;
+  }
+  &:focus {
+    border: 2px solid ${Theme.MAIN_COLOR3};
+    box-shadow: ${Theme.HOVER_SHADOW};
+  }
+`;
